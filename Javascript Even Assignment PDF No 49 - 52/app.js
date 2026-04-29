0aa0df09d8560f980e7d79a6a38009ef7017063a
Muhammadhasnain <webdevhasnain@gmail.com>
2026-04-29 16:09:35 +0500

    Add One task
    To show the data of inputs on the web page.

--- Javascript Even Assignment PDF No 49 - 52/app.js
+++ Javascript Even Assignment PDF No 49 - 52/app.js
@@ -1,15 +1,23 @@
 // Events Assignment //
 
 
-// let input1 = document.querySelector("#input1");
-// let input2 = document.querySelector("#input2");
-// let input3 = document.querySelector("#input3");
+let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+let input3 = document.querySelector("#input3");
 
-// let btn = document.querySelector("#btn");
+let btn = document.querySelector("#btn");
 
 
-// btn.addEventListener("click", function () {
-//     console.log("Name : " + input1.value);
-//     console.log("Father's Name : " + input2.value);
-//     console.log("Phone Number : " + input3.value);
-// })
+btn.addEventListener("click", function (ev) {
+    ev.preventDefault();
+    let div = document.createElement("div");
+    div.classList.add("div");
+    document.body.append(div)
+
+    div.innerHTML = `
+    Name : ${input1.value} <br>
+    Email : ${input2.value} <br>
+    Password : ${input3.value} <br  >
+    `
+
+})
